1861eed9acc328e67fe3490a213312fcff925bd8
Jerônimo Santos Rodrigues <santosominorej@gmail.com>
2026-03-12 16:43:28 -0300

    :sparkles: feat: Adding route to extracts page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Dashboard } from './pages/Dashboard';
 import { SimuladorVenda } from './pages/SimuladorVendas';
+import { Extrato } from './pages/Extrato';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/vendas" element={<SimuladorVenda />} />
+          <Route path='/transacoes' element={<Extrato />} />
           {/* Adicione outras rotas conforme criar as páginas */}
         </Routes>
       </main>
